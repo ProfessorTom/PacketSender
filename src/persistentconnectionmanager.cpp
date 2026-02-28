7db7a06bc8c47de407608dc470a2c80121cc2454
Tomas Gallucci <professortom@gmail.com>
2026-02-28 08:35:01 -0600

    add PersistentConnectionManager class files to project

---
+++ src/persistentconnectionmanager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tomas Gallucci on 2/28/26.
+//
+
+#include "persistentconnectionmanager.h"
---
+++ src/persistentconnectionmanager.h
@@ -0,0 +1,13 @@
+//
+// Created by Tomas Gallucci on 2/28/26.
+//
+
+#ifndef PERSISTENTCONNECTIONMANAGER_H
+#define PERSISTENTCONNECTIONMANAGER_H
+
+
+class PersistentConnectionManager {
+};
+
+
+#endif //PERSISTENTCONNECTIONMANAGER_H
